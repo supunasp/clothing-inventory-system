002e5c0c7c5e1b2b20fe17d6f84275c08d0404e6
Supun Athukorala <supunasp@gmail.com>
2026-05-25 00:36:19 +1000

    CSIS-26 : Manage User Accounts

--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,9 +1,19 @@
 const express = require('express');
-const {getAdminAnalytics} = require('../controllers/adminController');
+const {
+    getAdminAnalytics,
+    getUsers,
+    updateUserStatus,
+    updateUserRole,
+    deleteUser,
+} = require('../controllers/adminController');
 const {protect} = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 router.get('/analytics', protect, getAdminAnalytics);
+router.get('/users', protect, getUsers);
+router.put('/users/:id/status', protect, updateUserStatus);
+router.put('/users/:id/role', protect, updateUserRole);
+router.delete('/users/:id', protect, deleteUser);
 
 module.exports = router;
